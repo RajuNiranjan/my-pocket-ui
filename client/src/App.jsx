82d528c353b8cd02584453d31d43a0b539f5750b
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-06-07 21:06:50 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import SignIn from "./pages/Signin/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
+import About from "./pages/About/About";
+import Profile from "./pages/Profile/Profile";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign_in" element={<SignIn />} />
+        <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
