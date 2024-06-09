09948111d911c82e0474a6b22f314079d4abf7ca
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-06-09 09:51:28 +0530

    created signup page funtcionality

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 const SignUp = () => {
   const [signUpDetails, setSignUpDetails] = useState({
     userName: "",
@@ -7,7 +8,11 @@ const SignUp = () => {
     password: "",
   });
 
-  const onChnage = (e) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
     const { name, value } = e.target;
     setSignUpDetails({
       ...signUpDetails,
@@ -15,38 +20,73 @@ const SignUp = () => {
     });
   };
 
-  const handleSubmitSignUp = (e) => {
+  const handleSubmitSignUp = async (e) => {
     e.preventDefault();
-    console.log(signUpDetails);
-    setSignUpDetails({
-      userName: "",
-      email: "",
-      password: "",
-    });
+
+    // Validation
+    if (
+      !signUpDetails.userName ||
+      !signUpDetails.email ||
+      !signUpDetails.password
+    ) {
+      setError("All fields are required");
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/sign_up", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signUpDetails),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        setLoading(false);
+        setError(data.message || "Something went wrong");
+        return;
+      }
+
+      setLoading(false);
+      setError(null);
+      navigate("/sign_in");
+      setSignUpDetails({
+        userName: "",
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
   };
 
   return (
     <div className="flex justify-center items-center w-full">
       <div className="w-[25%] flex flex-col my-20 justify-center items-center">
-        <h1 className="font-bold  text-3xl">Sign Up</h1>
+        <h1 className="font-bold text-3xl">Sign Up</h1>
         <form
           onSubmit={handleSubmitSignUp}
-          className="flex flex-col gap-4 my-10 w-full ">
+          className="flex flex-col gap-4 my-10 w-full">
           <input
             name="userName"
             value={signUpDetails.userName}
             type="text"
             className="focus:outline-none border border-black rounded-md p-2"
-            placeholder="enter user name"
-            onChange={onChnage}
+            placeholder="Enter user name"
+            onChange={onChange}
           />
           <input
             name="email"
             value={signUpDetails.email}
             type="email"
-            className="focus:outline-none border border-black rounded-md  p-2"
-            placeholder="enter email"
-            onChange={onChnage}
+            className="focus:outline-none border border-black rounded-md p-2"
+            placeholder="Enter email"
+            onChange={onChange}
           />
           <input
             name="password"
@@ -54,21 +94,21 @@ const SignUp = () => {
             type="password"
             className="focus:outline-none border border-black rounded-md p-2"
             placeholder="********"
-            onChange={onChnage}
+            onChange={onChange}
           />
           <button
-            // onClick={handleSubmitSignUp}
             type="submit"
             className="bg-blue-500 text-white font-medium text-lg cursor-pointer rounded-md p-2">
-            Sing Up
+            {loading ? "Loading..." : "Sign Up"}
           </button>
-          <p>
-            already have an account?{" "}
-            <Link to="/sign_in">
-              <span className="cursor-pointer text-blue-600">singin</span>
-            </Link>
-          </p>
         </form>
+        <p>
+          Already have an account?{" "}
+          <Link to="/sign_in">
+            <span className="cursor-pointer text-blue-600">Sign in</span>
+          </Link>
+        </p>
+        {error && <p className="text-red-500 mt-5">{error}</p>}
       </div>
     </div>
   );
