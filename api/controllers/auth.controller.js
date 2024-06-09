1f4ed766fcbbe35bd79b03cf292b280e62b02ea6
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-06-09 10:56:37 +0530

    created signin router

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.models.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const singUp = async (req, res, next) => {
   try {
@@ -12,5 +13,27 @@ export const singUp = async (req, res, next) => {
     console.log(req.body);
   } catch (error) {
     next(errorHandler(500, "Email aready Existed"));
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "wrong email or password"));
+    }
+
+    const token = jwt.sign({ id: validUser?._id }, process.env.JWT_TOKEN);
+
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(errorHandler(500, "please chaeck email or password"));
   }
 };
--- api/routers/auth.router.js
+++ api/routers/auth.router.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { singUp } from "../controllers/auth.controller.js";
+import { signIn, singUp } from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/sign_up", singUp);
+authRouter.get("/sign_in", signIn);
 
 export default authRouter;
