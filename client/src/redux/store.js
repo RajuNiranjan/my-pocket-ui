5cdfc9a8b5675e56ca7a1e38644c3b44e8a61c9c
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-06-09 21:16:34 +0530

    added  redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({ user: userSlice });
+
+const persistorConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistorConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
