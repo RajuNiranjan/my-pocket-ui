446bb6c277ebac892abe2b050fc18d9379f843e9
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-06-15 22:22:34 +0530

    added cros middleware for locally run the backend

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import "../db_connect.js";
+import cors from "cors";
 import authRouter from "./routers/auth.router.js";
 dotenv.config();
 
-const PORT = process.env.PORT_NUMBER;
+const PORT = process.env.PORT_NUMBER || 3000;
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
