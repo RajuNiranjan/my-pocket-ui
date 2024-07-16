7dd32f2557df023b74515ec9ebad11b09b588420
Raju <rajuniranjan1910@gmail.com>
2024-07-16 10:21:12 +0530

    updated listing model

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -1,62 +1,59 @@
 import mongoose from "mongoose";
 
-const listingSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    address: {
-      type: String,
-      required: true,
-    },
-    regularPrice: {
-      type: Number,
-      required: true,
-    },
-    discountPrice: {
-      type: Number,
-      required: true,
-    },
-    bathRoom: {
-      type: Number,
-      required: true,
-    },
-    bedRoom: {
-      type: Number,
-      required: true,
-    },
-    furniture: {
-      type: Boolean,
-      // required: true,
-    },
-    parking: {
-      type: Boolean,
-      // required: true,
-    },
-    type: {
-      type: String,
-      required: true,
-    },
-    offer: {
-      type: Boolean,
-      // required: true,
-    },
-    imageURLS: {
-      type: Array,
-      required: true,
-    },
-    useRef: {
-      type: String,
-      required: true,
-    },
+const listingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  { timeseries: true, timestamps: true }
-);
+  description: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  regularPrice: {
+    type: Number,
+    required: true,
+  },
+  discountPrice: {
+    type: Number,
+    required: true,
+  },
+  bathRooms: {
+    type: Number,
+    required: true,
+  },
+  bedRooms: {
+    type: Number,
+    required: true,
+  },
+  furnitured: {
+    type: Boolean,
+    required: true,
+  },
+  parking: {
+    type: Boolean,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  offer: {
+    type: Boolean,
+    required: true,
+  },
+  imageURL: {
+    type: Array,
+    required: true,
+  },
+  useRef: {
+    type: String,
+    required: true,
+  },
+});
 const ListingModel = mongoose.model("Listing", listingSchema);
 
 export default ListingModel;
