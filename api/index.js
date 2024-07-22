e3c9083ff0fbf77a59135004aebb4fdeba96e6ee
Raju <rajuniranjan1910@gmail.com>
2024-07-22 11:02:32 +0530

    updated cors origin

--- api/index.js
+++ api/index.js
@@ -14,7 +14,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://express-react-frontend-flame.vercel.app",
     credentials: true,
   })
 );
