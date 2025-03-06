b71849aa9fcfd5dbd14bc361864c5bd4ff65645a
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-03-06 19:36:53 +0530

    initialized vite setup

--- api/routers/user.router.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { deleteUser, updateUser } from "../controllers/user.controller.js";
-import { verifyAccessToken } from "../../utils/verifyUser.js";
-
-export const userRoute = express.Router();
-
-userRoute.post("/updateUserInfo/:id", verifyAccessToken, updateUser);
-userRoute.delete("/deleteUser/:id", verifyAccessToken, deleteUser);
